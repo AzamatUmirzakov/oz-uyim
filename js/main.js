b77ab7c447092cf0a822c84752446e40a28c6ce3
azamatomato <azamat.umirzakoff@gmail.com>
2021-03-19 16:34:57 +0600

    finished stages

--- js/main.js
+++ js/main.js
@@ -12,3 +12,15 @@ for (let advantage of advantages.children) {
 }
 advantages.style.paddingLeft = margin + "px";
 advantages.style.paddingRight = margin + "px";
+
+let last_scroll = 0;
+window.addEventListener('scroll', () => {
+  console.log(true);
+  if (window.pageYOffset > last_scroll) {
+    document.querySelector('div.header-top').classList.remove('active');
+    last_scroll = window.pageYOffset;
+  } else {
+    document.querySelector('div.header-top').classList.add('active');
+    last_scroll = window.pageYOffset;
+  }
+})
